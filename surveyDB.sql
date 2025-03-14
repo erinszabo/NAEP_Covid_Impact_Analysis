826ca2377236eb3d06d8d4b2b55de718f884aaae
root <55733370+erinszabo@users.noreply.github.com>
2025-03-13 20:08:35 -0700

    Rename csv files and add to plan comments

--- surveyDB.sql
+++ surveyDB.sql
@@ -7,6 +7,7 @@ CREATE DATABASE surveyDB;
 ---  I think I'll also need to specify some things in the table creation.
 --CREATE TABLE mathTB
 
+
 --- here I want to create a table for the READING csv file data to go into
 ---  I think I'll also need to specify some things in the table creation.
 --CREATE TABLE readingTB
@@ -16,21 +17,24 @@ CREATE DATABASE surveyDB;
 ---   possibly using the folowing lines
 
 ---BULK INSERT mathTB
----FROM 'ltt-reading2022covidsurveyresults.csv'
+---FROM <filename>
+
+----- ^^ The above methods would require me to build the schema manually first
+-----    which I could do, but I'll try using csvkit first.
 
 ------------------------------------ SETUP: ---------------------------------------------------------
 
 -- docker build -t mysql_db .
 
 -- docker run -d mysql_db
--- ^^ -d so that it doesnt take up the terminal
+-- ^^ -d so that it doesn't take up the terminal
 
------- then list the containers so i can get the <containerID>
---docker container ls
+------ then list the containers so I can get the <containerID>
+-- docker container ls
 
 -- docker exec -it <containerID> /bin/bash
 
------- now should be running inside container. now you can cd into the dir created in the dockerfile 
+------ now should be running inside container, now you can cd into the dir created in the dockerfile 
 ------   and now we can access mysql:
 
 -- cd docker-entrypoint-initdb.d
@@ -42,10 +46,17 @@ CREATE DATABASE surveyDB;
 -- show tables
 ----- ^^ now we can see the tables in the database
 
+------------------------ create tables from csv files ---------------------------------
+
+-- csvsql --dialect mysql --snifflimit 100000 math_survey_results.csv > math_table.sql
+-- csvsql --dialect mysql --snifflimit 100000 reading_survey_results.csv > reading_table.sql
+
+-- show tables
+
+-- SELECT * from  <tablename>
+----^ do this with math and reading tables to check that they were created and populated correctly
+
 ---------------------------------------------------------------------------------------------------
 
 
 
-
--- SELECT * from  <tablename>
-----^ do this with math and reading tables to check that they were created and populated correctly
