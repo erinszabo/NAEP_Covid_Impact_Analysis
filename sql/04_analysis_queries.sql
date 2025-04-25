3c7a8aaf2b5b52725a55e07a27b9d8b72db42ea7
root <55733370+erinszabo@users.noreply.github.com>
2025-04-24 19:56:24 -0700

    Look at only complete data

--- sql/04_analysis_queries.sql
+++ sql/04_analysis_queries.sql
@@ -1,2 +1,17 @@
--- with the simplified tables, see what patterns we can find.
 SELECT 'Running transform script...' AS status;
+
+USE surveyDB;
+
+-- remove rows with missing information
+CREATE TABLE math_no_missing AS
+SELECT *
+FROM math_hc
+WHERE Question NOT LIKE '%MISSING%'
+    AND CategoryL NOT LIKE '%MISSING%';
+
+
+CREATE TABLE reading_no_missing AS
+SELECT *
+FROM reading_hc
+WHERE Question NOT LIKE '%MISSING%'
+    AND CategoryL NOT LIKE '%MISSING%';
