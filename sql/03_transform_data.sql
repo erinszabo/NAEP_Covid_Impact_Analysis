e1d12765da4475cdf81d394ef69146fac606e9b2
root <55733370+erinszabo@users.noreply.github.com>
2025-05-21 17:24:04 -0700

    Consolidate data transformation and remove analysis SQL file

--- sql/03_transform_data.sql
+++ sql/03_transform_data.sql
@@ -4,6 +4,7 @@ SELECT 'Running transform script...' AS status;
 USE surveyDB;
 
 -- to reduce size and ensure only high confidence (hc) data, only consider rows with high SE scores
+-- remove rows with missing information
 
 
 CREATE TABLE math_hc AS
@@ -16,7 +17,9 @@ SELECT
     Avg_score,
     Question
 FROM mathTB
-WHERE ScoreSE < 3 AND PCT_SE < 3;
+WHERE ScoreSE < 3 AND PCT_SE < 3
+    AND Question NOT LIKE '%MISSING%'
+    AND CategoryL NOT LIKE '%MISSING%';
 
 CREATE TABLE reading_hc AS
 SELECT
@@ -28,4 +31,9 @@ SELECT
     Avg_score,
     Question
 FROM readingTB
-WHERE ScoreSE < 3 AND PCT_SE < 3;
+WHERE ScoreSE < 3 AND PCT_SE < 3
+    AND Question NOT LIKE '%MISSING%'
+    AND CategoryL NOT LIKE '%MISSING%';
+
+
+
