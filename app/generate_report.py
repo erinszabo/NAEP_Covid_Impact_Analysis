ecc583eac00411742ef7647bb80b3cf044186da9
root <55733370+erinszabo@users.noreply.github.com>
2025-07-06 14:57:48 -0700

    Create add_paragraph function in gen report

--- app/generate_report.py
+++ app/generate_report.py
@@ -1,7 +1,12 @@
 from analyze_survey import analyze
 from create_visuals import visuals
+
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import Paragraph
+from reportlab.platypus import Frame
+from reportlab.lib.units import inch
 
 
 ########### Driver Function #############
@@ -11,17 +16,29 @@ def gen_doc():
     analyzed = analyze()
     figures = visuals(analyzed) # probably a list or dictionary of visuals
     
-    document, width, height = start_pdf()
+    doc, width, height = start_pdf()
+    
+    x_pos = 20 # starting bottom left x position
+    y_pos = .2 # starting bottom left y position  
+    
+    # # test add paragraph
+    # y_pos = add_paragraph (doc, TEST, x_pos, y_pos)
+    # y_pos = add_paragraph (doc, "TEST words words words", x_pos, y_pos)
+    # y_pos = add_paragraph (doc, "hbo;jknjpip7oftykcgvjhbkniy7pfotykcghvjbknhi;yfutdkxfchgvjbkhiogu;yfldturkxfhg j.vkblhioug;yfltyfhg.jvbkhogu;iyfutlcgjhkbjoug;yfitulfhgj.hkbjug;iyfutlc", x_pos, y_pos)
+    # y_pos = add_paragraph (doc, str(width)+"  "+str(height)+"    hbo;knhi;yfutdkxfchgvjbkhiogu;yfldturkxfhg j.vkblhioug;yfltyfhg.jvbkhogu;iyfutlcgjhkbjoug;yfitulfhgj.hkbjug;iyfutlc", x_pos, y_pos)
+    
     
     
     # finally, save the document as a pdf
-    document.save()
+    doc.save()
     
     return
 
 ############ String Constants ################
 INTRO = "" # write my introduction paragraph here
 CONCLUSION = "" 
+
+TEST = " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
 ##############################################
 
@@ -43,9 +60,45 @@ def start_pdf():
     
     return doc, width, height
 
-def add_paragraph():
-    # add paragraph to doc
-    pass
+def add_paragraph(doc, text, x, y, width=7.5*inch, height=10*inch):
+    """ Add a paragraph to the doc
+
+    Args:
+        doc (canvas): the doc the paragraph is to be added to
+        text (str): the text to be added
+        x (float): the horizontal distance from the left of the page
+        y (float): the vertical distance from the bottom of the page
+        width (float): the width of the text block (in points or inches)
+        height (float): the height of the text block (in points or inches)
+
+    Returns:
+        new_y (float): the new vertical distance from the bottom of the page for next item
+    """
+    
+    # set paragraph style
+    styles = getSampleStyleSheet()
+    style = styles["Normal"]
+    style.fontName = "Helvetica"
+    style.fontSize = 11
+    style.leading = 15  # line spacing
+
+    # create paragraph and frame
+    paragraph = Paragraph(text, style)
+    frame = Frame(x, y, width, height, showBoundary=0) # height does not seem to need dependence on text length
+
+    # wrap paragraph in a frame and draw it
+    frame.addFromList([paragraph], doc)
+    
+    # for every 95 chars (ceiling), I want a new line plus one gap line  (let a line be 14)
+    space = 14 * -(-len(text)//95) + 14
+    
+    new_y = y - space  # update vertical position for next addition to doc
+    
+    return new_y
+    
+    
+
+
 
 def list_top_sig_questions():
     # 
