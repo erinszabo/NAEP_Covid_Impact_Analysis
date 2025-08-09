e8b44ae87f825c5d590e25d02320e0ffa40c2aae
root <55733370+erinszabo@users.noreply.github.com>
2025-08-09 12:46:02 -0700

    Identify visuals to compare findings

--- app/create_visuals.py
+++ app/create_visuals.py
@@ -44,19 +44,46 @@ def most_sig(subject, sf_path):
     i = 1 # to keep track of the figure number
     for question in df_sig['Question'].unique():
         sub = df_sig[df_sig['Question'] == question]
-        x_labels = sub['CategoryL_low'] + ' vs ' + sub['CategoryL_high']
+        x_labels = (sub['CategoryL_low'] + ' vs ' + sub['CategoryL_high']).apply(lambda s: str(s).replace('\r', '').replace('\n', ''))
         x = range(len(sub))
         width = 0.35
-
+        
+        q_str = qs_from_highlights(question)
+        
+        # Create a bar plot
         plt.figure(figsize=(8, 5))
         plt.bar(x, sub['PercentA_low'], width=width, label='PercentA_low')
         plt.bar([xi + width for xi in x], sub['PercentA_high'], width=width, label='PercentA_high')
         plt.xticks([xi + width/2 for xi in x], x_labels, rotation=45)
-        plt.title(question.strip('" '))
+        plt.title(question.strip('" ').replace('\r', '').replace('\n', ''))
         plt.ylabel('Percent')
         plt.tight_layout()
         plt.legend()
-        plt.savefig(f"output/visuals/{subject}_most_significant_{i}.png")
+        plt.savefig(f"output/visuals/{q_str}_{subject}_ms_{i}.png")
         plt.close()
         i += 1
-    return # ["output/"+subject+"_most_significant_"+str(i)+".png" for i in range(1, i)]
+    return 
+
+def qs_from_highlights(q):
+    # check if the q matches one of the highlighted qs,
+    #  if so, return the substring that should be added to the file name
+    #  if not, return an empty string
+    q = q.strip()
+    
+    highlighted_qs = {
+        # these first 3 are highlighted from NAEP
+        'Remote: Access to high-speed internet': 'internet',
+        'Remote: desktop; laptop or tablet': 'device',
+        'Remote: A quiet place to work': 'quiet',
+        # these are from my own analysis
+        "Remote math: Recognize when don't understand": 'recognize',
+        'Remote math: Ask for help when you need it': 'ask',
+        "Remote E/LA: Recognize when don't understand": 'recognize',
+        "Remote E/LA: Ask for help when you need it": 'ask',
+    }
+
+    sub_string = ""
+    if q in highlighted_qs.keys():
+        sub_string = highlighted_qs[q]
+        
+    return sub_string
